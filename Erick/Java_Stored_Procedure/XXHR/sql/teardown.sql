4bf59e20ed8a7ac8f5e559873e389cab267e5c46
erickjones1 <erick.jones@flexagon.com>
2020-03-12 12:08:50 -0500

    fixed teardown script

--- Erick/Java_Stored_Procedure/XXHR/sql/teardown.sql
+++ Erick/Java_Stored_Procedure/XXHR/sql/teardown.sql
@@ -1,5 +1,5 @@
 drop table scott.LineItems;
 drop table scott.StockItems;
 drop table scott.Orders;
-drop table scot.Customers;
-drop package po_mgr;
+drop table scott.Customers;
+drop package scott.po_mgr;
