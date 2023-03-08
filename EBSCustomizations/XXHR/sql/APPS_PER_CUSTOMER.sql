abf8829df021e58ed6d5dd8d68a6fa2ad4b3e7e5
Flexagon9 <Flexagon9@gmail.com>
2023-03-08 19:18:01 +0530

    minor changes in script

--- EBSCustomizations/XXHR/sql/APPS_PER_CUSTOMER.sql
+++ EBSCustomizations/XXHR/sql/APPS_PER_CUSTOMER.sql
@@ -1,20 +1,22 @@
 -- Create USER PLSQL block
+set serveroutput on;
+/
 declare
   tableexists integer;
 begin
-  select count(*) into tableexists from all_tables where table_name='CUSTOMER';
+  select count(*) into tableexists from all_tables where table_name='CUSTOMER' and owner='APPS';
   if (tableexists = 0) then
     DBMS_OUTPUT.put_line('New table to be created.');
   else
      DBMS_OUTPUT.put_line('Deleting existing tables');
-     execute immediate 'DROP table Customer';
+     execute immediate 'DROP table APPS.Customer';
   end if;
 end;
 /
 --------------------------------------------------------
 --  DDL for Table Customer
 --------------------------------------------------------
-CREATE TABLE Customer
+CREATE TABLE apps.Customer
    (CUSTOMER_ID NUMBER(30), 
    Name VARCHAR2(20), 
    Age NUMBER(20), 
@@ -27,21 +29,21 @@ CREATE TABLE Customer
 --  DDL for Index Customer_PK
 --------------------------------------------------------
   /
-  CREATE UNIQUE INDEX Customer_PK ON Customer (CUSTOMER_ID) ;
+  CREATE UNIQUE INDEX Customer_PK ON apps.Customer (CUSTOMER_ID) ;
   
 --------------------------------------------------------
 --  Constraints for Table Customer
 --------------------------------------------------------
   /
-  ALTER TABLE Customer ADD CONSTRAINT Customer_PK PRIMARY KEY (CUSTOMER_ID)  ENABLE;
+  ALTER TABLE apps.Customer ADD CONSTRAINT Customer_PK PRIMARY KEY (CUSTOMER_ID)  ENABLE;
   /
-  ALTER TABLE Customer MODIFY (CUSTOMER_ID NOT NULL ENABLE);
+  ALTER TABLE apps.Customer MODIFY (CUSTOMER_ID NOT NULL ENABLE);
   /
-  Insert into Customer values(1,'Smith',31,'Sweden','Senior Developer');
+  Insert into apps.Customer values(1,'Smith',31,'Sweden','Senior Developer');
   /
-  Insert into Customer values(2,'Nio',40,'Norway','Architect');
+  Insert into apps.Customer values(2,'Nio',40,'Norway','Architect');
   /
-  Insert into Customer values(3,'Morvious',45,'Ireland','Senior Manager');
+  Insert into apps.Customer values(3,'Morvious',45,'Ireland','Senior Manager');
   /
-  Insert into Customer values(4,'Trinity',38,'Japan','Architect');
+  Insert into apps.Customer values(4,'Trinity',38,'Japan','Architect');
 
