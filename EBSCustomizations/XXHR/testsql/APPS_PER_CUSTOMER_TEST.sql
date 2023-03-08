00dfb1705e5b11e9ccd23ed312240f6744316982
Flexagon9 <Flexagon9@gmail.com>
2023-03-08 19:03:22 +0530

    UPdated to reference apps tables

--- EBSCustomizations/XXHR/testsql/APPS_PER_CUSTOMER_TEST.sql
+++ EBSCustomizations/XXHR/testsql/APPS_PER_CUSTOMER_TEST.sql
@@ -1,5 +1,5 @@
-create or replace package test_customer as
-  -- %suite(CustomerValidation)
+create or replace package   apps_test.test_customers  AUTHID CURRENT_USER as
+  -- %suite(CustomerValidations)
   
   -- %test(Check count of Architect)
   procedure check_architect;
@@ -9,19 +9,20 @@ create or replace package test_customer as
 end;
 /
 
-create or replace package body test_customer as
+create or replace package body apps_test.test_customers as
 
   procedure check_architect is
     Emp_count   INTEGER := 20;
   begin
-    select count(1) into Emp_count from Customer where upper(Designation)='ARCHITECT';
+    select count(1) into Emp_count from apps.Customer where upper(Designation)='ARCHITECT';
     ut.expect(Emp_count).to_equal(2);
   end;
   
   procedure check_Norway_employee is
     emp_name1 VARCHAR2(20);
   begin
-    select Name into emp_name1 from Customer where upper(Country)='NORWAY';
+    select Name into emp_name1 from apps.Customer where upper(Country)='NORWAY';
     ut.expect( emp_name1 ).to_equal('Nio');
   end;
 end;
+/
